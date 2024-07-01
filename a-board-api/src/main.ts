7969d5ca476d807cba994d2f8bad8441e3bd64fb
audyz <audy.work1208@gmail.com>
2024-07-02 00:46:28 +0700

    update config

--- a-board-api/src/main.ts
+++ a-board-api/src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.AIP_URL || 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
-bootstrap();
+
+export default bootstrap;
